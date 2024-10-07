4c2696ffb4591cee88f41625681a6410bd69d6f0
hyemin <hyemin_park@tmax.co.kr>
2024-10-07 15:55:41 +0900

    자바 실행하기

---
+++ src/HelloJava.java
@@ -0,0 +1,13 @@
+// 파일명과 클래스 이름이 같아야 한다.
+public class HelloJava { // 블록
+    // 실행 시점
+    // main 메서드 (메서드 vs 함수)
+    // 자바는 main(String[] args) 메서드를 찾아서 프로그램을 실행한다.
+    public static void main(String[] args) {
+        // 값을 콘솔에 출력한다.
+        // 자바는 한 문장이 끝날 때마다 세미콜론을 통해 구분한다.
+        System.out.println("hello java");
+    }
+}
+
+// 자바 언어는 대소문자를 구분
