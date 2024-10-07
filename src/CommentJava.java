da5d8f4f6ef00eaa8c871005edfc9731d78a70bf
miniddo <kg3718@naver.com>
2024-10-07 16:02:14 +0900

    주석(comment)

---
+++ src/CommentJava.java
@@ -0,0 +1,14 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        // hello java1을 실행합니다. 한 줄 주석
+        System.out.println("hello java1");
+
+        /*
+         * 여러 줄 주석
+         *
+         * System.out.println("hello java2");
+         * System.out.println("hello java3");
+         */
+    }
+}
